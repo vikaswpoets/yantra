8030948e27c73e92cd6c1c13e936edf00a74360f
vikaswpoets <125905737+vikaswpoets@users.noreply.github.com>
2024-08-29 21:40:42 +0530

    array functiion
    [array shortcode /]

--- Core/LowCode/Shortcodes/LArray.php
+++ Core/LowCode/Shortcodes/LArray.php
@@ -1,18 +1,14 @@
 <?php
 
-// Core/sh/TestShortcode.php
 namespace Core\LowCode\Shortcodes;
 
-use Core\LowCode\LowCode;
 use Core\LowCode\LowCodeModule;
 use Core\LowCode\LowCodeTemplate;
-use Core\LowCode\ModuleNotFoundException;
 use Core\LowCode\Shortcode;
 use Core\LowCode\ShortcodeManager;
 
 class LArray extends Shortcode
 {
-    public static string|null $shortcode_name = "array";
     public function __construct()
     {
         parent::__construct("array");
@@ -22,19 +18,57 @@ class LArray extends Shortcode
     {
         $this->parent = $parent;
         $this->elderSibling = $elderSibling;
-        $attr = $config['attributes']??[];
+        $attr = $config['attributes'] ?? [];
+        $action = $config['sections'][0] ?? '';
+        $default = $attr['default'] ?? '';
+        $output = '';
 
-        if(!empty($attr['main']))
-        {
-            $arr_name = $attr['main'];
-            $arr = ShortcodeManager::parseArray($content,$parent);
-            $this->set($arr_name, $arr);
-            return "";
+        switch ($action) {
+            case 'create':
+                $arr_name = $attr['main'];
+                $arr = ShortcodeManager::parseArray($content,$parent);
+                $this->set($arr_name, $arr);
+                break;
+            case 'jsondecode':
+                $jsonData = $this->replaceVariables($attr['data'] ?? $default);
+                $output = json_decode($jsonData, true);
+                break;
+
+            case 'arraypush':
+                $array = $this->get($attr['array']);
+                $value = $this->replaceVariables($attr['main'] ?? $default);
+                $array[] = $value;
+                $output = $array;
+                break;
+
+            case 'arrayslice':
+                $array = $this->get($attr['array']);
+                $offset = intval($attr['offset'] ?? 0);
+                $length = isset($attr['length']) ? intval($attr['length']) : null;
+                $output = array_slice($array, $offset, $length);
+                break;
+
+            case 'arraymerge':
+                $array1 = $this->get($attr['array1']);
+                $array2 = $this->get($attr['array2']);
+                $output = array_merge($array1, $array2);
+                break;
+
+            case 'arraysearch':
+                $array = $this->get($attr['array']);
+                $value = $this->replaceVariables($attr['main'] ?? $default);
+                $output = array_search($value, $array);
+                break;
+
+            default:
+                $output = $default;
+                break;
         }
-        else
-        {
-            throw new \Exception("Missing array name!");
+
+        if (isset($attr['set'])) {
+            $this->set($attr['set'], $output);
         }
+
+        return is_array($output) ? json_encode($output) : (string)$output;
     }
-
 }
