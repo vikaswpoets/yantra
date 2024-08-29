1a6ff850a593c070516ca20e0ee74198901db5bd
vikaswpoets <125905737+vikaswpoets@users.noreply.github.com>
2024-08-29 21:25:22 +0530

    More Language Shortcodes
    Get , App, data, date, string, mysqli,

--- Core/LowCode/Shortcodes/Get.php
+++ Core/LowCode/Shortcodes/Get.php
@@ -21,28 +21,50 @@ class Get extends Shortcode
         $this->parent = $parent;
         $this->elderSibling = $elderSibling;
 
-        $attr = $config['attributes']??[];
-        $default = $attr['default']??"";
+        $attr = $config['attributes'] ?? [];
+        $default = $attr['default'] ?? "";
 
         $value = "";
-        if(isset($attr['main'])){
-            $value = $this->get($attr['main'])??$default;
-            if(isset($attr['set'])){
-                $this->set($attr['set'],$value);
+
+        if (isset($attr['main'])) {
+            // Check if we are dealing with an object property or method
+            if (strpos($attr['main'], '.') !== false) {
+                list($objectName, $propertyOrMethod) = explode('.', $attr['main'], 2);
+                $object = $this->get($objectName);
+
+                if (is_object($object)) {
+                    if (method_exists($object, $propertyOrMethod)) {
+                        // Call the method on the object
+                        $value = call_user_func_array([$object, $propertyOrMethod], array_values($attr));
+                    } elseif (property_exists($object, $propertyOrMethod)) {
+                        // Access the property on the object
+                        $value = $object->$propertyOrMethod;
+                    } else {
+                        throw new \Exception("Method or property '{$propertyOrMethod}' does not exist in object '{$objectName}'.");
+                    }
+                } else {
+                    throw new \Exception("Object '{$objectName}' does not exist.");
+                }
+            } else {
+                // Previous functionality to get value from the environment or default
+                $value = $this->get($attr['main']) ?? $default;
+            }
+
+            // Set the value to another variable if needed
+            if (isset($attr['set'])) {
+                $this->set($attr['set'], $value);
                 return "";
             }
         }
-        if(is_string($value)){
+
+        // Handle different types of values
+        if (is_string($value)) {
             return $value;
-        }
-        elseif(is_array($value) ||is_object($value)){
-            var_dump($attr['main']);
+        } elseif (is_array($value) || is_object($value)) {
             return json_encode($value);
-        }
-        elseif(is_bool($value)){
-            return $value?"true":"false";
-        }
-        elseif(is_numeric($value)){
+        } elseif (is_bool($value)) {
+            return $value ? "true" : "false";
+        } elseif (is_numeric($value)) {
             return "$value";
         }
 
---
+++ Core/LowCode/Shortcodes/LData.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Core\LowCode\Shortcodes;
+
+use Core\LowCode\LowCodeModule;
+use Core\LowCode\LowCodeTemplate;
+use Core\LowCode\Shortcode;
+use Core\LowCode\ShortcodeManager;
+
+class LData extends Shortcode
+{
+    public function __construct()
+    {
+        parent::__construct("data");
+    }
+
+    public function parse(string $content, array $config, LowCodeTemplate | LowCodeModule | Shortcode $parent = null,  LowCodeTemplate | LowCodeModule | Shortcode  $elderSibling = null): string
+    {
+        $this->parent = $parent;
+        $this->elderSibling = $elderSibling;
+        $attr = $config['attributes'] ?? [];
+        $action = $config['sections'][0] ?? '';
+        $default = $attr['default'] ?? '';
+        $output = '';
+
+        switch ($action) {
+            case 'jsondecode':
+                $jsonData = $this->replaceVariables($attr['data'] ?? $default);
+                $output = json_decode($jsonData, true);
+                break;
+
+            case 'arraypush':
+                $array = $this->get($attr['array']);
+                $value = $this->replaceVariables($attr['value'] ?? $default);
+                $array[] = $value;
+                $output = $array;
+                break;
+
+            case 'arrayslice':
+                $array = $this->get($attr['array']);
+                $offset = intval($attr['offset'] ?? 0);
+                $length = isset($attr['length']) ? intval($attr['length']) : null;
+                $output = array_slice($array, $offset, $length);
+                break;
+
+            case 'arraymerge':
+                $array1 = $this->get($attr['array1']);
+                $array2 = $this->get($attr['array2']);
+                $output = array_merge($array1, $array2);
+                break;
+
+            case 'arraysearch':
+                $array = $this->get($attr['array']);
+                $value = $this->replaceVariables($attr['value'] ?? $default);
+                $output = array_search($value, $array);
+                break;
+
+            default:
+                $output = $default;
+                break;
+        }
+
+        if (isset($attr['set'])) {
+            $this->set($attr['set'], $output);
+        }
+
+        return is_array($output) ? json_encode($output) : (string)$output;
+    }
+}
